5b5aeabf2894e309b3d81c0e64c3a6d2c0b4a113
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-06-09 13:19:48 -0400

    added while loop and import statements

---
+++ main.py
@@ -0,0 +1,9 @@
+import saloon
+import store
+import stable
+
+while True:
+    print("What in tarnation?? How'd I get in this jail cell?")
+    question = input('What do you want to do?')
+    if question == False:
+        break
---
+++ store.py
@@ -0,0 +1,2 @@
+def test_store():
+    print('Store has printed!')
