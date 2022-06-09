8117e4e393ef069d6744ab314a5361633655a480
Levi Shevick <levidshevick@gmail.com>
2022-06-09 13:20:14 -0400

    adds test function for testing

---
+++ saloon.py
@@ -0,0 +1,10 @@
+
+def buy_drink():
+    choice = input('Pick a drink from the menu: 1: Beer, 2: Whiskey, 3: Margarita.')
+    if choice == 1:
+        print('You guzzle your hipstery IPA down like a fish out of water.')
+    if choice == 2:
+        print('You throw back the entire bottle of whiskey in three satisfying gulps.')
+    if choice == 3:
+        print('You sip on your Margarita like the classy scoundrel you are.')
+        
