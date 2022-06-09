448d56f4b37ddce83751b079f1868856f4d756ed
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-06-09 15:35:43 -0400

    added rules for faro game

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import store
 import stable
 from inventory import inventory
 
-inventory = { 'money': 0, 'disguise': None }
-
 # print description of how we ended up in the two square
 
 while True:
 
-    question = input("You're in the town square. Where do you want to go?\n1: Saloon\n2: Store\n3: Stable\n\nChoice: ")
+    question = input("\nYou're in the town square. Where do you want to go?\n1: Saloon\n2: Store\n3: Stable\n\nChoice: ")
     if question == '1':
         saloon.saloon_loop()
     elif question == '2':
--- saloon.py
+++ saloon.py
@@ -1,11 +1,13 @@
 from time import sleep
 
+sleep_val = 1
+
 def bar():
-    sleep(1)
+    sleep(sleep_val)
     print('You go up to the bartender...')
-    sleep(1)
+    sleep(sleep_val)
     drink = input('Bartenter - "Howdy, what can I get you? 1: Beer, 2: Whiskey, 3: Margarita.": ')
-    sleep(1)
+    sleep(sleep_val)
     if drink == '1':
         print('You guzzle your hipstery IPA down like a fish out of water.')
     elif drink == '2':
@@ -16,17 +18,32 @@ def bar():
         print('You down the water quickly for maximum hydration.')
 
 def faro_game():
-    print("You take a seat at the faro table. It's a quiet day so it's just you and the dealer")
-    sleep(2)
-    print("\nDealer - \"Welcome! Faro is popular game around these parts. Place your bets...\"")
+    print("""\nYou take a seat at the faro table. It's a quiet day so it's just you and the dealer.
+    \nDealer - \"Welcome! Faro is popular card game around these parts. Would you like me to explain the rules?\"""")
+    hear_rules()
 
+def hear_rules():
+    option = input("\n1: Sure! I'm a little rusty.\n2: No thanks, I know what I'm doing\n\nChoice: ")
+    if option == '1':
+        print("""\nDealer - Splendid! Here are the rules.\n
+        I will lay out 13 cards, each with a value 2 through ace.
+        Place bets on any card. You can place multiple bets at a time!
+        I will then draw two cards from the remaining deck.
+        First is the dealer card. If it has the same value as one you have money on, you lose that bet.
+        Second is the player's card. If you have bet on this number, you win the bet! 
+        If an unrelated card is drawn, your bet stays.
+        If both cards that are drawn are the same, you lose half of your bet. 
+        \nGot it? Great!
+        """)
+    elif option == '2':
+        print("Dealer - Well, alrighty then!")
 
 def saloon_loop():
-    sleep(1)
+    sleep(sleep_val)
     print("\nYou enter the saloon and look around. It reeks of smoke and booze.")
     while True:
-        where_to_go = input("Where do you want to go?\n1: Bar\n2: Faro table\n3: Back to town square: ")
-        sleep(1)
+        where_to_go = input("Where do you want to go?\n1: Bar\n2: Faro table\n3: Back to town square:\n\nChoice: ")
+        sleep(sleep_val)
         if where_to_go == '1':
             bar()
         elif where_to_go == '2':
