eff02ab5ee6d4f36de7a01fe7fce57a508f56aa2
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-06-09 15:29:24 -0400

    small update

--- stable.py
+++ stable.py
@@ -1,15 +1,20 @@
-import main
 from time import sleep
 
 def stable_loop():
-    print("You enter the stable. You see the stable master")
+    sleep(2)
+    print('\n' * 5)
+    print("You enter the stable. You see the stable master working, his back is turned")
     while True:
-
-        choice = input("Would you like to \n1: Speak to the stable master or \n2: Steal his horse?")
+        sleep(1)
+        choice = input("Would you like to \n1: Speak to the stable master, or \n2: Steal his horse while he isn't looking?  ")
         if choice == "1":
-                print("You approach the stable master. He seems shocked to see you even you before you speak. You ask him about buying a horse. 'I recognize you!' he says, 'I would be hung for selling a horse to a known criminal! Now leave!'")
+                sleep(2)
+                print('\n' * 5)
+                print("You approach the stable master. He seems shocked to see you even you before you speak. You ask him about buying a horse. 'I recognize you!' he says, 'I would be hung for selling a horse to a known criminal! Now leave!' You leave  ")
         if choice == "2":
-                print("You approach the horse, moving as quietly as you can. As you lift yourself onto the saddle you spook the horse, causing it to buck. The noise alerts the stable master, who is ready with his six-shooter. A few quick shots and you're dead.  Game Over. \nPress 1: to start over, \nPress 2: to exit")
+                sleep(2)
+                print('\n' * 5)
+                print("You approach the horse, moving as quietly as you can. As you lift yourself onto the saddle you spook the horse, causing it to buck. The noise alerts the stable master, who is ready with his six-shooter. A few quick shots and you're dead.  Game Over. \nPress 1: to start over, \nPress 2: to exit  ")
                 if choice == "1":
                         main()
                 else:
