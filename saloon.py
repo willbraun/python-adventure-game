03b2765bf986d4868a88c561f3a6a39411925c99
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-06-09 16:33:22 -0400

    started faro game logic

--- saloon.py
+++ saloon.py
@@ -1,6 +1,24 @@
 from time import sleep
+from random import shuffle
 
 sleep_val = 1
+
+def saloon_loop():
+    sleep(sleep_val)
+    print("\nYou enter the saloon and look around. It reeks of smoke and booze.")
+    while True:
+        where_to_go = input("Where do you want to go?\n1: Bar\n2: Faro table\n3: Back to town square:\n\nChoice: ")
+        sleep(sleep_val)
+        if where_to_go == '1':
+            bar()
+        elif where_to_go == '2':
+            faro_table()
+        elif where_to_go == '3':
+            break
+        else:
+            print("Sorry, didn't catch that")
+
+# Bar
 
 def bar():
     sleep(sleep_val)
@@ -17,18 +35,22 @@ def bar():
     else:
         print('You down the water quickly for maximum hydration.')
 
-def faro_game():
+# Faro table
+
+def faro_table():
     print("""\nYou take a seat at the faro table. It's a quiet day so it's just you and the dealer.
     \nDealer - \"Welcome! Faro is popular card game around these parts. Would you like me to explain the rules?\"""")
     hear_rules()
+    play_faro()
 
 def hear_rules():
     option = input("\n1: Sure! I'm a little rusty.\n2: No thanks, I know what I'm doing\n\nChoice: ")
     if option == '1':
         print("""\nDealer - Splendid! Here are the rules.\n
-        I will lay out 13 cards, each with a value 2 through ace.
-        Place bets on any card. You can place multiple bets at a time!
-        I will then draw two cards from the remaining deck.
+        You may place bets on any card value for ace, 2, all the way to king
+        You can place multiple bets at a time!
+        For the first turn, I will burn a card from the deck and show it face up.
+        Then for every turn afterwards, I will then draw two cards from the deck.
         First is the dealer card. If it has the same value as one you have money on, you lose that bet.
         Second is the player's card. If you have bet on this number, you win the bet! 
         If an unrelated card is drawn, your bet stays.
@@ -38,17 +60,45 @@ def hear_rules():
     elif option == '2':
         print("Dealer - Well, alrighty then!")
 
-def saloon_loop():
-    sleep(sleep_val)
-    print("\nYou enter the saloon and look around. It reeks of smoke and booze.")
-    while True:
-        where_to_go = input("Where do you want to go?\n1: Bar\n2: Faro table\n3: Back to town square:\n\nChoice: ")
-        sleep(sleep_val)
-        if where_to_go == '1':
-            bar()
-        elif where_to_go == '2':
-            faro_game()
-        elif where_to_go == '3':
-            break
-        else:
-            print("Sorry, didn't catch that")
+def play_faro():
+    card_values = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+    deck = create_deck(card_values)
+    bets = dict.fromkeys(card_values, 0)
+
+    burn_card(deck)
+
+
+
+    # Prompt player to make one bet on value and amount
+
+    # Put in loop so player can make multiple bets
+
+    # Deal dealer and player cards
+
+    # Win, lose, or split bets
+
+
+
+    
+    
+def create_deck(card_values):
+    deck = []
+    for _ in range(4):
+        deck.extend(card_values)
+    shuffle(deck)
+    return deck
+
+def draw_card(deck):
+    return deck.pop()
+
+def burn_card(deck):
+    print(f'\nBurned card: {draw_card(deck)}')
+
+
+
+    
+
+
+
+
+
