8a7713cbcf6c7a6a489d841838a94d116fd86e92
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-06-11 17:44:39 -0400

    upload

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import saloon
 import store
 import stable
+from time import sleep
 from general import *
 
 # print description of how we ended up in the two square
 
 while True:
 
-    question = input("\nYou're in the town square. Where do you want to go?\n1: Saloon\n2: Store\n3: Stable\n\nChoice: ")
+    print('\n'*20)
+    print('The harsh Colorado sunlight stings your eyes - but you welcome the stinging because it\'s the first sunlight you\'ve seen in days.')
+    sleep(6)
+    print('\n'*1)
+    print('You\'ve been in the town jail cell long enough and that sunlight means freedom.')
+    sleep(6)
+    print('\n'*1)
+    print('There are two things you know for sure: the first is when opportunity knocks you have to answer --- so when that forgetful sheriff left your cell door unlocked before taking his afternoon nap you answered.')
+    sleep(6)
+    print('\n'*1)
+    print('The second thing you know: you\'ve got to ESCAPE FROM DURANGO!, and fast!')
+    sleep(6)
+    print('\n'*5)
+    print("Looking across the town's muddy, horse trodden thoroughfare you see three buildings you can enter: the saloon, the general store and the horse stable. ")
+    question = input("\n Where do you want to go?\n1: Saloon\n2: Store\n3: Stable\n\nChoice: ")
     if question == '1':
         saloon.saloon_loop()
     elif question == '2':
--- stable.py
+++ stable.py
@@ -5,40 +5,198 @@ from general import *
 
 board = [' ' for x in range(10)]
 
-def printBoard(board):
-    print('   |   |')
+def choose_square(mark, position):
+    board[position] = mark
+
+def open_space(position):
+    return board[position] == ' '
+
+def print_board(board):
+    print('1  |2  |3')
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
     print('-----------')
-    print('   |   |')
+    print('4  |5  |6')
     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
     print('   |   |')
     print('-----------')
-    print('   |   |')
+    print('7  |8  |9')
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
 
-def player_turn():
-    pass
+def winner(bo, ma):
+    return (bo[7] == ma and bo[8] == ma and bo[9] == ma) or (bo[4] == ma and bo[5] == ma and bo[6] == ma) or(bo[1] == ma and bo[2] == ma and bo[3] == ma) or(bo[1] == ma and bo[4] == ma and bo[7] == ma) or(bo[2] == ma and bo[5] == ma and bo[8] == ma) or(bo[3] == ma and bo[6] == ma and bo[9] == ma) or(bo[1] == ma and bo[5] == ma and bo[9] == ma) or(bo[3] == ma and bo[5] == ma and bo[7] == ma)
 
-def computer_turn():
-    pass
+def user_move():
+    run = True
+    while run:
+        print("\n"*5)
+        move = input(f'{set_color("Stable Master", red)}: Picka square an\' mark it with \'X\' (1-9): ')
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if open_space(move):
+                    run = False
+                    choose_square('X', move)
+                else:
+                    print("\n"*2)
+                    print(f'{set_color("Stable Master", red)}: I dun picked that one!')
+            else:
+                print("\n"*2)
+                print(f'{set_color("Stable Master", red)}: Pick 1 through 9!')
+        except:
+            print("\n"*2)
+            print('Please type a number!')
 
-def turn():
-    pass
+def computer_move():
+    possibleMoves = [x for x, mark in enumerate(board) if mark == ' ' and x != 0]
+    move = 0
+
+    for let in ['O', 'X']:
+        for i in possibleMoves:
+            boardCopy = board[:]
+            boardCopy[i] = let
+            if winner(boardCopy, let):
+                move = i
+                return move
+
+    cornersOpen = []
+    for i in possibleMoves:
+        if i in [1,3,7,9]:
+            cornersOpen.append(i)
+            
+    if len(cornersOpen) > 0:
+        move = random_selection(cornersOpen)
+        return move
+
+    if 5 in possibleMoves:
+        move = 5
+        return move
+
+    edgesOpen = []
+    for i in possibleMoves:
+        if i in [2,4,6,8]:
+            edgesOpen.append(i)
+            
+    if len(edgesOpen) > 0:
+        move = random_selection(edgesOpen)
+        
+    return move
+
+def random_selection(li):
+    import random
+    ln = len(li)
+    r = random.randrange(0,ln)
+    return li[r]
+    
+
+def isBoardFull(board):
+    if board.count(' ') > 1:
+        return False
+    else:
+        return True
+
+def the_end():
+    sleep(3)
+    print("\n"*10)
+    print("You win")
+    exit()
+
+def coin_flip():
+    while True:
+        print(f'{set_color("Stable Master", red)}: Yur choice, call it in the air: heads or tails?')
+        print("He flips the coin- ")
+        sleep(2)
+        print("\n"*5)
+        guess = input("Press 1 for 'heads'\nPress 2 for 'tails': ")
+        if(guess == "1"):
+            print("\n 'I choose heads'")
+        elif(guess == "2"):
+            print("\n 'I choose tails.'")
+        else:
+            print(f'\n{set_color("Stable Master", red)}: That ain\'t no choice!!')
+        flip = random.choice(["1", "2"])
+        
+        if(flip == "1"):
+                sleep(2)
+                print("\nThe coin hits the ground - on heads")
+        else:
+                sleep(2)
+                print("\nThe coin hits the ground - on tails")
+        
+        if(flip == guess):
+                sleep(2)
+                print(f'\n{set_color("Stable Master", red)}: You won ... I guess I misjudged ya. Here\'s yer ticket for the stage coach.')
+                the_end()
+        else:
+            sleep(2)
+            print(f'\n{set_color("Stable Master", red)}: Ha! Not so lucky for you huh? I\'ll tell ya what. I\'ll give you another chance.')
+        break                  
+
+
+def ttt_game():
+        sleep(5)
+        print("\n"*5)
+        print(f'{set_color("Stable Master", red)}: It\'s the wildest game in the West - it\'s called: Tic Tac Toe!')
+        sleep(5)
+        print('\n'*5)
+        global board
+        print_board(board)
+
+        while not(isBoardFull(board)):
+            if not(winner(board, 'O')):
+                user_move()
+                print("\n"*2)
+                sleep(3)
+                print_board(board)
+            else:
+                print(f'{set_color("Stable Master", red)}: Haha! I win!')
+                break
+
+            if not(winner(board, 'X')):
+                move = computer_move()
+                if move == 0:
+                    sleep(2)
+                    print("\n"*2)
+                    print(f'{set_color("Stable Master", red)}: Tie game?? Drats! Well... how about a different game? This is the second wildest game in the West. It\'s called: flip-a-coin!')
+                    sleep(5)
+                    print("\n"*5)
+                    coin_flip()
+                else:
+                    choose_square('O', move)
+                    print("\n"*2)
+                    sleep(3)
+                    print(f'{set_color("Stable Master", red)}: Let\'s see... Imma pick', move,': ')
+                    print("\n"*2)
+                    sleep(3)
+                    print_board(board)
+            else:
+                print(f'{set_color("Stable Master", red)}: You won ... I guess I misjudged ya. Here\'s yer ticket for the stage coach.')
+                the_end()
+
+            
+
+        while True:
+            answer = input(f'{set_color("Stable Master", red)}: Ya wanna play again??\'\nPress1: for yes\nPress2: for no')
+            if answer == "1":
+                ttt_game()
+            else:
+                exit()
+
 
 def stable_loop():
     if not "The Disguise Kit" in inventory["disguise"]:
         sleep(2)
-        print('\n' * 5)
+        print('\n' * 20)
         print("You enter the stable. You see the stable master working, his back is turned")
         while True:
 
-            choice = input("Would you like to: \n1: Speak to the stable master\n2: Steal his horse while he isn't looking?  ")
+            choice = input("Would you like to: \n1: Speak to the stable master\n2: Steal his horse while he isn't looking?\n3: Leave  ")
             if choice == "1":
                     sleep(2)
                     print('\n' * 5)
-                    print("You approach the stable master. He seems shocked to see you even you before you speak. You ask him about buying a horse. 'I recognize you!' he says, 'I would be hung for selling a horse to a known criminal! Now leave!'")
+                    print("You approach the stable master. He seems shocked to see you even you before you speak. You ask him about buying a horse.")
+                    print(f'{set_color("Stable Master", red)}: I recognize you!! I\'d be hung for selling a horse to a known criminal! Now leave!')
                     sleep(5)
                     print("\n"*5)
                     print("You leave.")
@@ -58,20 +216,22 @@ def stable_loop():
                         exit()
                     else:
                         print("That's not a choice")
+            if choice == "3":
+                break
     else:    
-                sleep(5)
+                sleep(3)
                 print('\n' * 20)
                 print("You enter the stable. The stable master sees you and approaches.")
                 sleep(5)
                 print("\n"*5)
-                print("'Well, I've never seen you around here before.  You must be new in town. How can I help you?'")
+                print(f'{set_color("Stable Master", red)}: Well, I\'ve never seen you around here before.  You must be new in town. How can I help you?')
                 while True:
                     sleep(5)
                     print("\n"*3)
                     print("'I need a ticket for the first stagecoach leaving town.'")
                     sleep(5)
                     print("\n"*3)
-                    print("'Leaving so soon? Can't say I blame you. It'll be $10'")
+                    print(f'{set_color("Stable Master", red)}: Leaving so soon? Can\'t say I blame you. It\'ll be $10')
                     sleep(5)
                     print("\n"*3)
                     print("'I don't have $10...'")
@@ -84,53 +244,14 @@ def stable_loop():
                         print("Are you a gamblin' man?")
                         sleep(5)
                         print("\n"*3)
-                        print("Why yes I am. An' I know just the game we can play...")
-                        break
+                        print(f'{set_color("Stable Master", red)}: Why yes I am. An\' I know just the game we can play...')
+                        ttt_game
                     else:
                         sleep(10)
                         print("\n"*5)
                         print("... I ... I really need this ticket. I'm not a begging man typically but today I am.  Please, find it in your heart to help me out.")
                         sleep(10)
                         print("\n"*5)
-                        print("'Quit yer blubberin'. What if made a wager. If you win you get yer ticket, if I win ...")
-                        break 
-                    # change breaks to conitinue when finihsed with game
-
-
-def game():
-    while True:
-        print("'Yur choice, call it in the air: heads or tails?' He flips the coin- ")
-        sleep(2)
-        print("\n"*5)
-        guess = input("Press 1 for 'heads'\nPress 2 for 'tails': ")
-        if(guess == "1"):
-            print("\n 'I choose heads'")
-        elif(guess == "2"):
-            print("\n 'I choose tails.'")
-        else:
-            print("\n'That ain't no choice!!'")
-        flip = random.choice(["1", "2"])
-        
-        if(flip == "1"):
-                sleep(2)
-                print("The coin hits the ground - on heads")
-        else:
-                sleep(2)
-                print("The coin hits the ground - on tails")
-        
-        if(flip == guess):
-                sleep(2)
-                print("'Fine... you win, you go first.'")
-        else:
-            sleep(2)
-            print("'Ha! Not off to a good start, are you? I'll go first.'")
-        break
-    # while True:
-    #     if(flip == guess):
-    #         player_turn() = turn
-    #     else:
-    #         computer_turn() = turn
-    #     break
-
-            
+                        print(f'{set_color("Stable Master", red)}: Quit yer blubberin\'. What if made a wager. If you win you get yer ticket, if I win I want (PLACEHOLDER)...')
+                        ttt_game()          
 
